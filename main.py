3a5e669a01c396d91555631860d6653ee0bd3cc3
Bryan-Ferreiraaa <josyetecandido@gmail.com>
2026-03-08 13:02:45 -0300

    Refactor main.py to use render_template and update routes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template
 from PIL import Image
 import io
-import os
 
 app = Flask(__name__)
 
-# O "CÓDIGO SECRETO" XMP que o Facebook procura
+# Metadados XMP que "ativam" o giro 360 no Facebook/Google
 XMP_TAG = (
+    '<?xpacket begin="" id="W5M0MpCehiHzreSzNTczkc9d"?>'
     '<x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.1.0-jc003">'
     '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">'
     '<rdf:Description rdf:about="" xmlns:GPano="http://ns.google.com/photos/1.0/panorama/">'
@@ -19,34 +19,33 @@ XMP_TAG = (
     '<GPano:CroppedAreaLeftPixels>0</GPano:CroppedAreaLeftPixels>'
     '<GPano:CroppedAreaTopPixels>0</GPano:CroppedAreaTopPixels>'
     '</rdf:Description></rdf:RDF></x:xmpmeta>'
+    '<?xpacket end="r"?>'
 )
 
 @app.route('/')
 def index():
-    return '''
-        <h1>Conversor 360 Profissional</h1>
-        <form action="/convert" method="post" enctype="multipart/form-data">
-            <input type="file" name="image" accept="image/*">
-            <button type="submit">Converter para 360° Real</button>
-        </form>
-    '''
+    return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
+    if 'image' not in request.files:
+        return "Nenhuma imagem enviada", 400
+    
     file = request.files['image']
     img = Image.open(file.stream)
     
-    # 1. Força a proporção 2:1 (4096x2048)
+    # Converte para RGB (evita erro com PNG transparente)
     img = img.convert("RGB")
+    
+    # Redimensiona para 4096x2048 (Proporção de Ouro 2:1)
     img_360 = img.resize((4096, 2048), Image.Resampling.LANCZOS)
     
-    # 2. Salva em um buffer na memória
     img_io = io.BytesIO()
-    # Injeta o XMP nos metadados do JPEG (isso o JS não faz direito)
+    # SALVA INJETANDO O XMP (O segredo que o JavaScript não consegue fazer)
     img_360.save(img_io, 'JPEG', quality=90, xmp=XMP_TAG)
     img_io.seek(0)
     
-    return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='FOTO_360_REAL.jpg')
+    return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='PANO_360_REAL.jpg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
